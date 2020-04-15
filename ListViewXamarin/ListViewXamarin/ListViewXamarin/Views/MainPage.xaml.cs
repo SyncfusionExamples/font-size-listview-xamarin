bfe16ae11d7435b760b6238c6a422f2ea3072070
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-15 10:59:18 +0530

    XAMARIN-34036 - Sample and screen shot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Views/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Syncfusion.DataSource;
+using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        #region Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            this.SizeChanged += MainPage_SizeChanged;
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void MainPage_SizeChanged(object sender, EventArgs e)
+        {
+            DefineFontSize("FontSizeLarge", 38, 30);
+            DefineFontSize("FontSizeBody", 48, 42);
+            DefineFontSize("FontSizeSmall", 52, 48);
+        }
+
+        private void DefineFontSize(string resourceKey, int charsInLine, int linesInPage)
+        {
+            var size1 = Math.Round(Width / charsInLine) * 2;
+            var size2 = Math.Round(Height / linesInPage);
+            App.Current.Resources[resourceKey] = Math.Min(size1, size2);
+        }
+        #endregion
+    }
+
+}
